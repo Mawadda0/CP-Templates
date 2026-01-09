2f7c684404ad64fca15d96b1e9e5fb0ea248d695
Mawchin <memegaper200@gmail.com>
2026-01-09 20:06:57 +0200

    bfs & dfs template on map

---
+++ Graph Theory/BFS-DFS.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define nl '\n'
+#define ll long long
+#define int long long
+#define fixed(n) fixed << setprecision(n)
+#define sz(x) (int)(x.size())
+#define all(v) v.begin(), v.end()
+#define F first
+#define S second
+#define min_count(x) order_of_key(x)
+#define num_in_pos(x) find_by_order(x)
+#define ull unsigned long long
+
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <class T>
+using ordered_multiset = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// "سُبْحَانَكَ لا عِلْمَ لَنَا إِلَّا مَا عَلَّمْتَنَا إِنَّكَ أَنْتَ الْعَلِيمُ الْحَكِيمُ"
+
+
+map<int, vector<int>> gr;
+vector<bool> vis;
+
+void dfs_itr(int node) // dfs iterative
+{
+    stack<int> st;
+    st.push(node);
+    vis[node] = 1;
+    while(!st.empty())
+    {
+        int tp = st.top();
+        st.pop();
+        cout << tp << " ";
+        for(int i : gr[tp])
+        {
+            if(!vis[i])
+            {
+                st.push(i);
+                vis[i] = 1;
+            }
+        }
+    }
+    cout << nl;
+}
+
+void dfs_rec(int node) // dfs recursive
+{
+    vis[node] = 1;
+    cout << node << " ";
+    for(int i : gr[node])
+    {
+        if(!vis[i])
+        {
+            dfs_rec(i);
+        }
+    }
+}
+
+void bfs_itr(int node) // bfs iterative
+{
+    queue<int> q;
+    q.push(node);
+    vis[node] = 1;
+    while(!q.empty())
+    {
+        int tp = q.front();
+        q.pop();
+        cout << tp << " ";
+        for(int i : gr[tp])
+        {
+            if(!vis[i])
+            {
+                q.push(i);
+                vis[i] = 1;
+            }
+        }
+    }
+    cout << nl;
+}
+
+void bfs_rec(int node) // bfs recursive
+{
+    // Still thinking how to implement...
+}
+
+
+void solve()
+{
+    gr.clear(), vis.clear();
+
+    int n, m; cin >> n >> m;
+    vis.assign(n + 1, false);
+
+    int op = m;
+    while(op--)
+    {
+        int x, y; cin >> x >> y;
+        gr[x].push_back(y);
+        gr[y].push_back(x);
+    }
+
+    auto it = gr.begin();
+    int root = gr.begin()->F;
+
+    dfs_itr(root);
+    fill(all(vis), false);
+
+    dfs_rec(root);
+    fill(all(vis), false);
+
+    cout << nl;
+    bfs_itr(root);
+    fill(all(vis), false);
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int tc = 1;
+    //cin >> tc;
+    while (tc--)
+    {
+        solve(); 
+        if(tc) cout << nl;
+    }
+    return 0;
+}
